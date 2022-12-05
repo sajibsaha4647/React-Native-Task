95635bdfcb301838e0d22a6c9a2c4b84ff1f4f61
sajibsaha4647 <sksaha4648@gmail.com>
2022-12-05 17:03:03 +0600

    add navigation

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './Src/Redux/Store';
-import Navigatoin from './Src/Screens/Navigation';
+import Navigation from './Src/Screens/Navigation';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <Provider store={store}>
-        <Navigatoin />
+        <Navigation />
       </Provider>
     </SafeAreaView>
   );
--- Src/Screens/Navigation.js
+++ Src/Screens/Navigation.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
- 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-});
- 
-const Navigatoin = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Navigatoin</Text>
-        </View>
-    );
-}
+import React, {Component} from 'react';
+import {Dimensions, View, Text, Image, Platform} from 'react-native';
+let width = Dimensions.get('window').width;
+let height = Dimensions.get('window').height;
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
- 
-export default Navigatoin;
+import SplashScreen from './SplashScreen';
+import HomeScreen from './HomeScreen';
+
+const MainStack = createNativeStackNavigator();
+const MainScreen = () => {
+  return (
+    <MainStack.Navigator screenOptions={{headerShown: false}}>
+      <MainStack.Screen name="SplashScreen" component={SplashScreen} />
+      <MainStack.Screen name="HomeScreen" component={HomeScreen} />
+    </MainStack.Navigator>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <MainScreen />
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
