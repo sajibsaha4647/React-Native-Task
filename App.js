78721b134f36a17be82945628d9dc04509efb54a
sajibsaha4647 <sksaha4648@gmail.com>
2022-12-05 16:56:07 +0600

    add redux files

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux'
-import { Store } from 'redux';
+import store from './Src/Redux/Store';
+import Navigatoin from './Src/Screens/Navigation';
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
- 
+
 const App = () => {
   return (
-    <SafeAreaView style={{ flex:1 }}>
-      <View>
-        <Text>App</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <Provider store={store}>
+        <Navigatoin />
+      </Provider>
     </SafeAreaView>
-    
   );
-}
+};
 
  
 export default App;
