f6ecc289526b2d3509a03bc81e829ac20d42436d
sajibsaha4647 <sksaha4648@gmail.com>
2022-12-06 10:43:38 +0600

    home screen design

--- Src/Component/HeaderComponent.js
+++ Src/Component/HeaderComponent.js
@@ -4,13 +4,25 @@ import {View, Text, StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f54263',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 const HeaderComponent = () => {
   return (
     <View style={styles.container}>
-      <Text>HeaderComponent</Text>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View
+          style={{
+            justifyContent: 'center',
+          }}>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>
+            Move The Ball In The Screen
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
--- Src/Screens/HomeScreen.js
+++ Src/Screens/HomeScreen.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
- 
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import HeaderComponent from '../Component/HeaderComponent';
+let width = Dimensions.get('window').width;
+let height = Dimensions.get('window').height;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+  },
 });
- 
+
 const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>HomeScreen</Text>
-        </View>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <View style={{height: width * 0.136}}>
+        <HeaderComponent />
+      </View>
+      <View></View>
+    </View>
+  );
+};
 
  
 export default HomeScreen;
